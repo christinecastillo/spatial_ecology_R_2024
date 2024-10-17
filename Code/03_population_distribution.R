b0c2875505ef4d50cf7aa6746327204b4a579ad6
Christine Castillo <chris.castillo0305@gmail.com>
2024-10-17 08:52:44 -0500

    Update 03_population_distribution.R

--- Code/03_population_distribution.R
+++ Code/03_population_distribution.R
@@ -1,9 +1,11 @@
 # Why populations disperse over the landscape in a certain manner?
 
+install.packages("terra")
 library(sdm)
 library(terra)
 
 file <- system.file("external/species.shp", package="sdm")
+# [1] "C:/Users/chris/AppData/Local/R/win-library/4.4/sdm/external/species.shp"
 
 rana <- vect(file)
 rana$Occurrence
@@ -11,11 +13,18 @@ rana$Occurrence
 plot(rana)
 
 # Selecting presences
-pres <- rana[rana$Occurrence==1,]
+pres <- rana[rana$Occurrence==1,]   #SQL language uses double == and everything end with , 
+plot(pres)
+pres$Occurrence
+
+par(mfrow=c(1,2))
+plot(rana)
 plot(pres)
 
 # exercise: select absence and call them abse
 abse <- rana[rana$Occurrence==0,]
+abse
+dev.off()
 plot(abse)
 
 # exrecise: plot presences and absences, one beside the other
@@ -29,6 +38,8 @@ dev.off()
 # exercise: plot pres and abse altogether with two different colours
 plot(pres, col="dark blue")
 points(abse, col="light blue")
+plot(pres, col="blue")
+points(abse, col="red")
 
 # predictors: environmental variables
 # file <- system.file("external/species.shp", package="sdm")
@@ -36,8 +47,16 @@ points(abse, col="light blue")
 
 # elevation predictor
 elev <- system.file("external/elevation.asc", package="sdm") 
+elev
+# [1] "C:/Users/chris/AppData/Local/R/win-library/4.4/sdm/external/elevation.asc"
+
 elevmap <- rast(elev) # from terra package
 plot(elevmap)
+cl <- colorRampPalette(c("black", "salmon", "pink", "purple"))(100)
+plot(elevmap, col="cl")
+clcl <- colorRampPalette(c("black", "lightskyblue", "midnightblue", "darkolivegreen4"))(100)
+plot(elevmap, col=clcl)
+
 points(pres, cex=.5)
 
 # temperature predictor
@@ -45,6 +64,11 @@ temp <- system.file("external/temperature.asc", package="sdm")
 tempmap <- rast(temp) # from terra package
 plot(tempmap)
 points(pres, cex=.5)
+points(pres, pch=19)
+
+
+
+
 
 # exrcise: do the same with vegetation cover
 vege <- system.file("external/vegetation.asc", package="sdm") 
